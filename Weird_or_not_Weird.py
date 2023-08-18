ed2e2f6ce5b503a27ce0e93a77e319ecf13eda89
vishwanadh2004 <119720593+vishwanadh2004@users.noreply.github.com>
2023-08-18 15:43:28 +0530

    n=int(input()) a=n%2 if a==1:     print("weird") elif a==0 in range (2,5):     print("not weird") elif a==0 in range (6,20):     print("weird") else:     print("not weird")

---
+++ Weird_or_not_Weird.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=n%2
+if a==1:
+    print("weird")
+elif a==0 in range (2,5):
+    print("not weird")
+elif a==0 in range (6,20):
+    print("weird")
+else:
+    print("not weird")
+    
+
+    
+    
